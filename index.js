024ba3220ed35f24de6dbdf030dfdd58982586f7
Atul Kumar Jha <atuljha21542@gmail.com>
2026-01-15 21:59:23 +0530

    Initial commit - Nodejs event driven chat app

---
+++ index.js
@@ -0,0 +1,25 @@
+const ChatRoom = require("./chatRoom.js")
+
+const chat = new ChatRoom()
+
+chat.on('join', (user) => {
+    console.log(`${user} has joined the chat`)
+});
+chat.on("message", (user, message) => {
+    console.log(`${user}: ${message}`);
+});
+chat.on("leave", (user) => {
+    console.log(`${user} has left the chat`);
+});
+
+//  Now simulate the chat
+
+chat.join('Atul')
+chat.join('Aman')
+
+chat.sendMessage("Atul", "hey Aman, How are you?");
+chat.sendMessage("Aman", "Hello Atul, i am doing fine, What about you?");
+
+chat.leave("Aman");
+chat.sendMessage("Aman", "This message won't be sent");
+chat.leave("Atul")
